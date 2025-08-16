7e6aa0281dda5648e20642d1f8a6e10ace84d9d0
D <bieler15147@live.de>
2025-08-16 17:51:23 +0200

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,63 +1,67 @@
 plugins {
-	kotlin("jvm") version "2.2.0"
-	kotlin("plugin.serialization") version "2.2.0"
-	application
+    kotlin("jvm") version "2.2.0"
+    kotlin("plugin.serialization") version "2.2.0"
+    `maven-publish`
 }
 
-application {
-	mainClass.set("dbdata.MainKt")
-}
-
-group = "de.bieler.dabiframework"
+group = "com.github.<IhrBenutzername>.DaBi-Framework"
 version = "1.0-SNAPSHOT"
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("io.github.classgraph:classgraph:4.8.165")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	testImplementation(kotlin("test"))
-	testImplementation("org.mockito:mockito-core:5.12.0")
-	testImplementation("org.mockito.kotlin:mockito-kotlin:5.3.1")
-	implementation("org.json:json:20231013")
+    implementation("io.github.classgraph:classgraph:4.8.165")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation(kotlin("test"))
+    testImplementation("org.mockito:mockito-core:5.12.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.3.1")
+    implementation("org.json:json:20231013")
 
-	// exposed
-	implementation("org.jetbrains.exposed:exposed-core:1.0.0-beta-5")
-	implementation("org.jetbrains.exposed:exposed-jdbc:1.0.0-beta-5")
-	implementation("org.jetbrains.exposed:exposed-java-time:1.0.0-beta-5")
-	implementation("com.h2database:h2:2.2.224")
+    // exposed
+    implementation("org.jetbrains.exposed:exposed-core:1.0.0-beta-5")
+    implementation("org.jetbrains.exposed:exposed-jdbc:1.0.0-beta-5")
+    implementation("org.jetbrains.exposed:exposed-java-time:1.0.0-beta-5")
+    implementation("com.h2database:h2:2.2.224")
 
-	// mongodb
-	implementation(platform("org.mongodb:mongodb-driver-bom:5.5.1"))
-	implementation("org.mongodb:mongodb-driver-kotlin-coroutine")
-	implementation("org.mongodb:mongodb-driver-kotlin-sync")
-	implementation("org.mongodb:bson-kotlinx")
+    // mongodb
+    implementation(platform("org.mongodb:mongodb-driver-bom:5.5.1"))
+    implementation("org.mongodb:mongodb-driver-kotlin-coroutine")
+    implementation("org.mongodb:mongodb-driver-kotlin-sync")
+    implementation("org.mongodb:bson-kotlinx")
 
-	// ktor-server
-	implementation("io.ktor:ktor-server-status-pages:3.2.2")
-	implementation("io.ktor:ktor-server-content-negotiation:3.2.2")
-	implementation("io.ktor:ktor-server-core:3.2.2")
-	implementation("io.ktor:ktor-server-cors:3.2.2")
-	implementation("io.ktor:ktor-server-netty:3.2.2")
-	implementation("io.ktor:ktor-server-config-yaml:3.2.2")
-	implementation("io.ktor:ktor-serialization-kotlinx-json:3.2.2")
+    // ktor-server
+    implementation("io.ktor:ktor-server-status-pages:3.2.2")
+    implementation("io.ktor:ktor-server-content-negotiation:3.2.2")
+    implementation("io.ktor:ktor-server-core:3.2.2")
+    implementation("io.ktor:ktor-server-cors:3.2.2")
+    implementation("io.ktor:ktor-server-netty:3.2.2")
+    implementation("io.ktor:ktor-server-config-yaml:3.2.2")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:3.2.2")
 
-	// ktor-client
-	implementation("io.ktor:ktor-client-core:3.2.2")
-	implementation("io.ktor:ktor-client-cio:3.2.2")
-	implementation("io.ktor:ktor-client-websockets:3.2.2")
+    // ktor-client
+    implementation("io.ktor:ktor-client-core:3.2.2")
+    implementation("io.ktor:ktor-client-cio:3.2.2")
+    implementation("io.ktor:ktor-client-websockets:3.2.2")
 }
 
 tasks.test {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 kotlin {
-	jvmToolchain(21)
-	tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-		compilerOptions {
-			freeCompilerArgs.addAll("-Xjsr305=strict", "-java-parameters")
-		}
-	}
+    jvmToolchain(21)
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        compilerOptions {
+            freeCompilerArgs.addAll("-Xjsr305=strict", "-java-parameters")
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
 }
