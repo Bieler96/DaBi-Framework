ddcf2e5fc51625309e900855866a63d0f0a9bc46
D <bieler15147@live.de>
2025-12-02 19:43:15 +0100

    update

--- src/main/kotlin/server/OpenApi.kt
+++ src/main/kotlin/server/OpenApi.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.json.JsonElement
 
 @Serializable
 data class OpenAPI(
-    val openapi: String = "3.0.3",
+    val openapi: String = "3.0.0",
     var info: Info,
     var paths: MutableMap<String, PathItem> = mutableMapOf()
 )
